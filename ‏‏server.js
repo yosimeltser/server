0630d540a28f8e782a76f3390714715b75dcce89
unknown <yosefmel@post.bgu.ac.il>
2018-05-19 12:10:19 +0300

    register works

--- ‏‏server.js
+++ ‏‏server.js
@@ -14,12 +14,25 @@ var port = 4000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
+
 app.post('/register',function (req,res){
-    console.log(req.body.City); 
-    DButilsAzure.execQuery("Insert Into [registeredUsers]  ([FirstName], [LastName],[City] ,[Country] ,[Email] ,[Categories] ,[Verifiers] ,[Username] , [UserPass])"
-    +"VALUES ("+req.body.FirstName+","+req.body.LastName+","+req.body.City+","+req.body.Countr+","+req.body.Email+","+req.body.Categories+","+req.body.Verifiers+","+ req.body.Username+","+req.body.UserPass+")")
+    //URI values
+    var FirstName = req.body.FirstName;
+    var LastName = req.body.LastName; 
+    var City = req.body.City; 
+    var Country = req.body.Country;
+    var Email = req.body.Email; 
+    var Categories = req.body.Categories;
+    var Verifiers = req.body.Verifiers;
+    var Username = req.body.Username;
+    var UserPass=req.body.UserPass;
+    //query
+    var sql= "INSERT INTO  [registeredUsers]  ([FirstName],[LastName],[City],[Country] ,[Email],[Categories] ,[Verifiers] ,[Username], [UserPass])" 
+    + " VALUES('"+FirstName+"','"+LastName+"','"+City+"','" +Country+"','"+Email+"','"+Categories+"','"+Verifiers+"','"+Username+"','"+UserPass+"')"; 
+    //execute in azure DB
+    DButilsAzure.execQuery(sql)
     .then(function(result){
-        res.send(result);
+        res.send(true);
     })
     .catch(function(err) {
         console.log(err);
