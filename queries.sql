dfa9798971c48c436cd0f171b4a2e6d0d9b0a946
unknown <yosefmel@post.bgu.ac.il>
2018-05-22 19:19:23 +0300

    login works

--- queries.sql
+++ queries.sql
@@ -1,16 +1,19 @@
-GO
-CREATE TABLE registeredUsers (
-	FirstName varchar(255),
-      LastName varchar(255),
-      City varchar(255),
-      Country varchar(255),
-      Email VARCHAR(255),
-      Categories varchar(255),
-	Verifiers varchar(255),
-	Username varchar(255), 
-	UserPass varchar(255)
-);
-GO
+-- GO
+-- CREATE TABLE registeredUsers (
+-- 	FirstName varchar(255),
+--       LastName varchar(255),
+--       City varchar(255),
+--       Country varchar(255),
+--       Email VARCHAR(255),
+--       Categories varchar(255),
+-- 	Verifiers varchar(255),
+-- 	Username varchar(255), 
+-- 	UserPass varchar(255)
+-- );
+-- GO
 -- GO 
 -- DROP TABLE registeredUsers
 -- GO
+GO 
+    SELECT * FROM registeredUsers  
+GO
--- ‏‏server.js
+++ ‏‏server.js
@@ -14,7 +14,8 @@ var port = 4000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
-
+//register for our page 
+//insert new row into the DB
 app.post('/register',function (req,res){
     //URI values
     var FirstName = req.body.FirstName;
@@ -32,7 +33,25 @@ app.post('/register',function (req,res){
     //execute in azure DB
     DButilsAzure.execQuery(sql)
     .then(function(result){
-        res.send(true);
+        res.send(result);
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+});
+app.post('/login', function (req,res){
+    var Username = req.body.Username;
+    var UserPass=req.body.UserPass;
+    var sql= "SELECT COUNT(*) FROM registeredUsers  WHERE Username='"+Username+"' AND UserPass= '"+UserPass+"';"
+    DButilsAzure.execQuery(sql)    
+    .then(function(result){
+        var counter=JSON.parse(result[0][""]);
+        if (counter==1) {
+            res.send('You are logged in now');
+        }
+        else {
+            res.send('Your username or password is incorrect');
+        }
     })
     .catch(function(err) {
         console.log(err);
