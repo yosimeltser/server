8ffb911f56f93130d701b34e3aa156cc7855b527
unknown <yosefmel@post.bgu.ac.il>
2018-05-22 21:48:43 +0300

    token works

--- ‏‏server.js
+++ ‏‏server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
+var jwt    = require('jsonwebtoken'); 
 app.use(cors());
 var DButilsAzure = require('./DButils');
 
@@ -14,6 +15,7 @@ var port = 4000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
+var  superSecret = "AKAunbreakable";
 //register for our page 
 //insert new row into the DB
 app.post('/register',function (req,res){
@@ -46,8 +48,15 @@ app.post('/login', function (req,res){
     DButilsAzure.execQuery(sql)    
     .then(function(result){
         var counter=JSON.parse(result[0][""]);
-        if (counter==1) {
-            res.send('You are logged in now');
+        if (counter==2) {
+                //the user is registered, create a token for him. 
+                var payload= {Username: Username}
+                var token= jwt.sign(payload,superSecret, {expiresIn: "1d" });
+                res.json({
+                    success: true, 
+                    message: 'Enjoy your token!',
+                    teken: token
+                });
         }
         else {
             res.send('Your username or password is incorrect');
@@ -56,7 +65,44 @@ app.post('/login', function (req,res){
     .catch(function(err) {
         console.log(err);
     })
+ 
+    
 });
+// route middleware to verify a token
+app.use('/reg', function (req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, superSecret, function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                // get the decoded payload and header
+                var decoded = jwt.decode(token, {complete: true});
+                req.decoded= decoded;
+                console.log(decoded.header);
+                console.log(decoded.payload)
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+
+})
 
 
 
