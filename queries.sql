91cfb2ac765bfb644fdf36809d158ac0a38adf94
Zohar <zoharavr@post.bgu.ac.il>
2018-05-25 00:08:08 +0300

    eqvo0pg2

--- queries.sql
+++ queries.sql
@@ -1,19 +1,50 @@
 -- GO
--- CREATE TABLE registeredUsers (
--- 	FirstName varchar(255),
---       LastName varchar(255),
---       City varchar(255),
---       Country varchar(255),
---       Email VARCHAR(255),
---       Categories varchar(255),
--- 	Verifiers varchar(255),
--- 	Username varchar(255), 
--- 	UserPass varchar(255)
+-- CREATE TABLE registeredUsers
+-- (
+--     Username varchar(8) NOT NULL PRIMARY KEY,
+--     FirstName varchar(255),
+--     LastName varchar(255),
+--     City varchar(255),
+--     Country varchar(255),
+--     Email VARCHAR(255),
+--     UserPass varchar(10)
 -- );
 -- GO
 -- GO 
 -- DROP TABLE registeredUsers
 -- GO
 GO 
-    SELECT * FROM registeredUsers  
+    SELECT * FROM Questions  
 GO
+-- 
+-- GO
+-- CREATE TABLE Categories(
+--     Category VARCHAR(255) NOT NULL,
+--     FK_Username VARCHAR (8) NOT NULL FOREIGN KEY REFERENCES registeredUsers(Username) ,
+--     CONSTRAINT PK_Categories PRIMARY KEY (Category,FK_Username),
+
+
+-- );
+-- GO
+-- GO
+-- CREATE TABLE Questions(
+--     Question VARCHAR(255) NOT NULL,
+--     Answer VARCHAR(255),
+--     FK_Username VARCHAR (8) NOT NULL FOREIGN KEY REFERENCES registeredUsers(Username) ,
+--     CONSTRAINT PK_Questions PRIMARY KEY (Question,FK_Username)
+-- );
+-- GO
+-- GO
+-- INSERT INTO registeredUsers(Username,FirstName,LastName,
+-- City,Country,Email,UserPass)
+-- VALUES ('zoharavr','Zohar','Avraham',
+-- 'New York','USA','zoharavr"post.bgu.ac.il','z123456');
+-- GO
+-- GO
+-- INSERT INTO  Questions(Question,Answer,FK_Username)
+-- VALUES ('What is your favorite animal','panda','zoharavr');
+-- GO
+
+
+
+
--- ‏‏server.js
+++ ‏‏server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
-var jwt    = require('jsonwebtoken'); 
+var jwt = require('jsonwebtoken');
 app.use(cors());
 var DButilsAzure = require('./DButils');
 
 //only registered users can operate functions on the file regUsers. 
-var signInUsers= require ('./routes/signInUsers')
-app.use('/onBoard',regUsers);
+var signInUsers = require('./routes/signInUsers')
+app.use('/onBoard', regUsers);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,66 +22,123 @@ var port = 4000;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
-var  superSecret = "AKAunbreakable";
+var superSecret = "AKAunbreakable";
 app.use(function (req, res, next) {
 
     console.log("server got request");
     next();
 
 });
-
+//check that the user name & the password meet the criteria
+function check_input(user, password) {
+    var ans = {
+        flag: "",
+        message: ""
+    };
+    if (/^[a-zA-Z]+$/.test(user)) {
+        if (user.length >= 2 && user.length <= 8) {
+            if (/^[a-z0-9]+$/.test(password)) {
+                if (password.length <= 10 && password.length >= 5) {
+                    ans.flag = true;
+                }
+                else {
+                    ans.flag = false;
+                    ans.message = "Password must be between 5 and 10"
+                }
+            }
+            else {
+                ans.flag = false;
+                ans.message = "Password must contain only letters and digits"
+            }
+        }
+        else {
+            ans.flag = false;
+            ans.message = "User name must be between 2 and 8 letters"
+        }
+    }
+    else {
+        ans.flag = false;
+        ans.message = "User name must contain only letters";
+    }
+    return ans;
+}
 //register for our page 
 //insert new row into the DB
-app.post('/register',function (req,res){
+app.post('/register', function (req, res) {
     //URI values
     var FirstName = req.body.FirstName;
-    var LastName = req.body.LastName; 
-    var City = req.body.City; 
+    var LastName = req.body.LastName;
+    var City = req.body.City;
     var Country = req.body.Country;
-    var Email = req.body.Email; 
+    var Email = req.body.Email;
     var Categories = req.body.Categories;
     var Verifiers = req.body.Verifiers;
     var Username = req.body.Username;
-    var UserPass=req.body.UserPass;
+    var UserPass = req.body.UserPass;
     //query
-    var sql= "INSERT INTO  [registeredUsers]  ([FirstName],[LastName],[City],[Country] ,[Email],[Categories] ,[Verifiers] ,[Username], [UserPass])" 
-    + " VALUES('"+FirstName+"','"+LastName+"','"+City+"','" +Country+"','"+Email+"','"+Categories+"','"+Verifiers+"','"+Username+"','"+UserPass+"')"; 
-    //execute in azure DB
-    DButilsAzure.execQuery(sql)
-    .then(function(result){
-        res.send(result);
-    })
-    .catch(function(err) {
-        console.log(err);
-    })
+    ans = check_input(Username,UserPass);
+    if(ans[0]){
+    var sql = "INSERT INTO  [registeredUsers]  ([Username],[FirstName],[LastName],[City],[Country] ,[Email], [UserPass])"
+        + " VALUES('" + Username + "','" + FirstName + "','" + LastName + "','" + City + "','" + Country + "','" + Email + "','" + UserPass + "')";
+        DButilsAzure.execQuery(sql)
+        .then(function (result) {
+           // res.send(result);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+    for(let i=0;i<Categories.length;i++){
+        let category_q="INSERT INTO  [Categories] ([Category],[FK_Username])"+
+        " VALUES ('" + Categories[i] + "','" + Username + "' )";
+        DButilsAzure.execQuery(category_q)
+        .then(function (result) {
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+    }
+    // for(let i=0;i<Verifiers.length;i++){
+    //     let verifyy_q="INSERT INTO  [Categories] ([Category],[FK_Username])"+
+    //     " VALUES ('" + Categories[i] + "','" + Username + "' )";
+    //     DButilsAzure.execQuery(verifyy_q)
+    //     .then(function (result) {
+    //     })
+    //     .catch(function (err) {
+    //         console.log(err);
+    //     })
+    // }
+    }
+    else {
+        res.send(ans[1]);
+    }
 });
 //when you are loggen in you wre onboard, because we are creating a teken for a session (24H).
-app.post('/login', function (req,res){
+app.post('/login', function (req, res) {
     var Username = req.body.Username;
-    var UserPass=req.body.UserPass;
-    var sql= "SELECT COUNT(*) FROM registeredUsers  WHERE Username='"+Username+"' AND UserPass= '"+UserPass+"';"
-    DButilsAzure.execQuery(sql)    
-    .then(function(result){
-        var counter=JSON.parse(result[0][""]);
-        if (counter==1) {
+    var UserPass = req.body.UserPass;
+    var sql = "SELECT COUNT(*) FROM registeredUsers  WHERE Username='" + Username + "' AND UserPass= '" + UserPass + "';"
+    DButilsAzure.execQuery(sql)
+        .then(function (result) {
+            var counter = JSON.parse(result[0][""]);
+            if (counter == 1) {
                 //the user is registered, create a token for him. 
-                var payload= {Username: Username}
-                var token= jwt.sign(payload,superSecret, {expiresIn: "1d" });
+                var payload = { Username: Username }
+                var token = jwt.sign(payload, superSecret, { expiresIn: "1d" });
                 res.json({
-                    success: true, 
+                    success: true,
                     message: 'Enjoy your token!',
                     teken: token
                 });
-        }
-        else {
-            res.send('Your username or password is incorrect');
-        }
-    })
-    .catch(function(err) {
-        console.log(err);
-    })
- 
-    
+            }
+            else {
+                res.send('Your username or password is incorrect');
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+
+
 });
 // route middleware to verify a token
 app.use('/onBoard', function (req, res, next) {
@@ -98,8 +155,8 @@ app.use('/onBoard', function (req, res, next) {
             } else {
                 // if everything is good, save to request for use in other routes
                 // get the decoded payload and header
-                var decoded = jwt.decode(token, {complete: true});
-                req.decoded= decoded;
+                var decoded = jwt.decode(token, { complete: true });
+                req.decoded = decoded;
                 console.log(decoded.header);
                 console.log(decoded.payload)
                 next();
